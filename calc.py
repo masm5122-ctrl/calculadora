a7efc0cc1d9204342195fc42cb852d4fc281e7c1
masm5122-ctrl <masm5122@gmail.com>
2025-11-26 10:57:25 -0300

    add: metodo add number
    Esta funcionando o metodo de add numero e a virgula

--- calc.py
+++ calc.py
@@ -1,11 +1,12 @@
 from PyQt5.QtWidgets import QMainWindow, QShortcut
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QKeySequence
+from funcoes import somar
 
 
 class CalcUI(QMainWindow):
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs): 
         super().__init__(**kwargs)
         loadUi("calculadora_iphone.ui", self)
         self.show()
@@ -22,7 +23,20 @@ class CalcUI(QMainWindow):
         self.btn_9.clicked.connect(lambda: self.addNumber(9))
         self.btn_0.clicked.connect(lambda: self.addNumber(0))
 
+        self.btn_virgula.clicked.connect( self.addComma)
+      
         self.btn_limpar.clicked.connect(self.cleanDisplay)
+        self.btn_igual.clicked.connect(self.showResult)
+
+
+    def addComma(self):
+        ultimo = self.display.text()
+        if ultimo.count(",") > 0:
+            resultado = ultimo
+        else:
+            resultado = ultimo + ","
+        self.display.setText(resultado)
+
 
     def addNumber(self, numero):
         primeiro = self.display.text()
@@ -34,4 +48,23 @@ class CalcUI(QMainWindow):
 
     def cleanDisplay(self):
         self.display.setText("0")
+
+    def showResult(self):
+        num1 = self.display.text()
+        if "," in num1:
+            num1 = num1.replace(',','.')
+            num1 = float(num1)
+        else:
+            num1 = int(num1)
+
+
+
+        num2 = 2     
+        result = somar(num1,num2)
+        print(f'Numero:{result}')
+        print("tipo:", type(result))
+
+    
+
+
         
---
+++ funcoes.py
@@ -0,0 +1,22 @@
+
+
+def somar(num1, num2):
+    resultado = num1 + num2
+    return resultado
+
+
+def subtracao(num1, num2):
+    resultado = num1 - num2
+    return resultado
+
+
+def multiplicacao(num1, num2):
+    resultado = num1 * num2
+    return resultado
+
+
+def dividir(num1, num2):
+    if num2 == 0:
+        return "Erro divisao"
+    return num1/num2
+
